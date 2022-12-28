2ce516024b0c248aaaac9f5e3eec7ecdcdfb418f
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2022-12-28 17:29:17 -0300

    He agregado en la libreria el archivo mysql-connector-j-8.0.31jar para que pueda conectarse la aplicacion con la base de datos, y ahora si me permite acceder el login a la ventana principal

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -7,11 +7,19 @@ import java.sql.SQLException;
 
 
 
+//try {
+//    Class.forName("com.mysql.jdbc.Driver");
+//} catch (ClassNotFoundException ex) {
+//    System.out.println("Error al registrar el driver de MySQL: " + ex);
+//}
+
 public class Conexion {
     
     Connection con;
     public Connection getConnection(){
+        
         try{
+            
             String myBD="jdbc:mysql://localhost:3306/sistemaventa?serverTimezone=UTC";
             con=DriverManager.getConnection(myBD,"root","");
             return con;
